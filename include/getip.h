05f3ed5e3d0e5fe61cd8d27af526b2753a3e8e92
jose03pedro <up202108673@edu.fe.up.pt>
2023-12-11 16:27:30 +0000

    Some things

--- include/getip.h
+++ include/getip.h
@@ -6,7 +6,7 @@
 
 #define h_addr h_addr_list[0]
 
-struct hostent {
+typedef struct {
     char *h_name;  // Official name of the host.
     char **
         h_aliases;  // A NULL-terminated array of alternate names  for the host.
@@ -16,8 +16,21 @@ struct hostent {
     char **h_addr_list;  // A zero-terminated array of network
                          //  addresses for the host.
                          // Host addresses are in Network Byte Order.
-};
+} hostent;
+
+typedef struct {
+    char host[512];
+    char user[256];
+    char password[256];
+    char path[512];
+    char fileName[512];
+    char host_name[512];
+    char ip[20];
+} urlArgs;
 
 void getIP(char *hostName);
+int url_parse(char *url, urlArgs *parsedUrl);
+int write_to_server(int socket, char *message);
+int connection_server(int socket, char *user, char *password);
 
 #endif  // GETIP_H
